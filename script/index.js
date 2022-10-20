7f021e85cc043db3d1a44b7c6f57376277d7cae8
khamudom <khamu@outlook.com>
2022-10-19 23:05:14 -0700

    add scroll behavior and email form functionality

--- script/index.js
+++ script/index.js
@@ -8,3 +8,17 @@ navLinks.forEach((link) =>
     link.classList.add("active");
   })
 );
+
+function SendMail() {
+  var params = {
+    from_name: document.getElementById("fullName").value,
+    subject_id: document.getElementById("subject_id").value,
+    email_id: document.getElementById("email_id").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_1f6c35r", "template_0le7g7c", params)
+    .then(function (res) {
+      alert("Message sent successfully!");
+    });
+}
